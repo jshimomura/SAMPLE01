47453324f0bc9b87bb9963af5cce0be149f30be3
j.shimomura <j.shimomura@yamajyo.co.jp>
2026-02-25 16:49:51 +0900

    引数ありのサンプル追加

--- src/test/App2.java
+++ src/test/App2.java
@@ -3,6 +3,10 @@ public class App2 {
     public static void main(String[] args) throws Exception {
         //コメコメ
         //修正コメ
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!!!");
+        String name = args[0];
+        String com = args[1];
+        System.out.println("args1:"+name);
+        System.out.println("args2:"+com);
     }
 }
